886c9ee253eeedf9ca068f8276fac4e35253e83c
MyHobbyAccount <142298998+MyHobbyAccount@users.noreply.github.com>
2024-12-06 13:06:37 +0100

    Update src/components/FAQ/FAQContent.jsx [BoltSync]

---
+++ src/components/FAQ/FAQContent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FAQList from './FAQList';
+
+const FAQContent = () => {
+  return (
+    <div className="w-full md:w-1/2 md:pl-8">
+      <FAQList />
+    </div>
+  );
+};
+
+export default FAQContent;
